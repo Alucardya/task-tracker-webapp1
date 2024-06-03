6964e322c81baf05c9498692ece45fa045989dcc
Alucardya <vladkhalilov@yahoo.com>
2024-06-03 08:52:20 +0300

    Deploy updated server configuration

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require('path');
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'task-tracker-client/build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'task-tracker-client/build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('App is listening on port ' + port);
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
