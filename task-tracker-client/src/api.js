9f8248ab92f2cbc25c4dc0271b115922aeb89bfb
Alucardya <vladkhalilov@yahoo.com>
2024-06-03 11:17:27 +0300

    Add backend functionality for task management

--- task-tracker-client/src/api.js
+++ task-tracker-client/src/api.js
@@ -1,13 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+function App() {
+    const [tasks, setTasks] = useState([]);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
 
-export const fetchTasks = async () => {
-  const response = await axios.get(`${API_URL}/tasks`);
-  return response.data;
-};
+    useEffect(() => {
+        axios.get('/tasks')
+            .then(response => setTasks(response.data))
+            .catch(error => console.log(error));
+    }, []);
 
-export const addTask = async (task) => {
-  const response = await axios.post(`${API_URL}/tasks`, task);
-  return response.data;
-};
+    const addTask = () => {
+        axios.post('/tasks', { title, description })
+            .then(response => {
+                setTasks([...tasks, { title, description, completed: false }]);
+                setTitle('');
+                setDescription('');
+            })
+            .catch(error => console.log(error));
+    };
+
+    return (
+        <div>
+            <h1>Task Tracker</h1>
+            <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            <button onClick={addTask}>Add Task</button>
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index}>{task.title}: {task.description}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
